e87bbab923c7f80f91616ec3e213645cb9edcfb6
equindar <equindar@gmail.com>
2025-02-27 23:39:40 +0100

    chore(db): added database structure issue: #117

--- backend/src/controllers/status/status.ts
+++ backend/src/controllers/status/status.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from 'express';
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import { configDatabase } from 'database/config';
 
-export function getStatus(_req: Request, res: Response) {
+const connection = await mysql.createConnection({
+  host: configDatabase.host,
+  user: configDatabase.user,
+  database: configDatabase.name,
+  port: configDatabase.port,
+  password: configDatabase.password
+});
+
+const db = drizzle({ client: connection });
+
+export async function getStatus(_req: Request, res: Response) {
   res.setHeader("Content-Type", "application/json");
   res.appendHeader("API-Version", "1.0")
   res.json({msg: "haha"});
