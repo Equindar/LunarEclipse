206ea950b44f98e6a35730d2bf51a4984fb8ced7
Equindar <equindar@gmail.com>
2025-09-16 11:38:46 +0200

    chore(repo): small changes

--- backend/src/controllers/status/status.ts
+++ backend/src/controllers/status/status.ts
@@ -2,9 +2,11 @@ import app from 'app';
 import { Request, Response } from 'express';
 
 export async function getStatus(_req: Request, res: Response) {
+
+  let value: number = Math.floor(Math.random() * 100);
   res.setHeader("Content-Type", "application/json");
   res.appendHeader("API-Version", "1.0")
-  res.json({msg: "haha"});
+  res.json({ online: true, latency: value });
   res.statusCode = 200;
 };
 
@@ -12,5 +14,5 @@ export async function getStatus(_req: Request, res: Response) {
 //   res.setHeader("Content-Type", "application/json");
 //   res.appendHeader("API-Version", "1.0")
 //   res.json({msg: "haha"});
-//   res.statusCode = 200; 
-// });
+//   res.statusCode = 200;
+// }); 
